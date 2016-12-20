efbd7eac8d8d3ebcc7362d64e90d2b8fd376f150
david <carranciodavid@gmail.com>
2016-12-20 18:51:03 +0100

    Solucionado error apis viejas

--- app/src/main/java/com/example/adrian/monumentos/GlobalState.java
+++ app/src/main/java/com/example/adrian/monumentos/GlobalState.java
@@ -1,17 +1,15 @@
 package com.example.adrian.monumentos;
 
-        import android.app.Application;
-
+import android.support.multidex.MultiDexApplication;
 import java.util.ArrayList;
 
 /**
  * Clase que se utilizará para almacenar la información global a toda la Aplicación.
  * En este caso, el ArrayList de POIs a mostrar
  */
-class GlobalState extends Application{
+public class GlobalState extends MultiDexApplication {
 
     private ArrayList<POI> listaPOIs;
-
 
     @Override
     public void onCreate() {
